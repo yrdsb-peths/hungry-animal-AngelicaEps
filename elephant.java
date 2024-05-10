01ad6aae3fec4a6a518bb472cbafdb4613156381
AngelicaEps <348821562@gapps.yrdsb.ca>
2024-05-10 15:49:45 -0400

    slowed elephant animation

--- elephant.java
+++ elephant.java
@@ -17,6 +17,7 @@ public class Elephant extends Actor
     GreenfootImage[] idleLeft = new GreenfootImage[8];
     
     String facing = "right";
+    SimpleTimer animationTimer = new SimpleTimer();
     
     public Elephant()
     {
@@ -32,10 +33,17 @@ public class Elephant extends Actor
             idleLeft[i].mirrorHorizontally();
             idleLeft[i].scale(100,100);
         }
+        animationTimer.mark();
+        
     }
     int imageIndex = 0;
     public void animateElephant()
     {
+        if(animationTimer.millisElapsed() < 100)
+        {
+            return; 
+        }
+        animationTimer.mark();
         if(facing.equals("right"))
         {
             setImage(idleRight[imageIndex]);
